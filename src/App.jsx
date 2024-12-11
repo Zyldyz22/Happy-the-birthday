3f677f9ba8e5e5a777aafdd671fa48eab0741241
zhyldyz <zyldyzabdyrahmanovo@gmail.com>
2024-12-11 17:55:33 +0600

    create hb

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      
+      
+      <h1>Happy birthday to you,Johnketti!</h1>
+      <iframe width="480" height="270" src="https://dzen.ru/embed/v3ytv0CwD3wU?from_block=partner&from=zen&mute=0&autoplay=0&tv=0" allow="autoplay; fullscreen; 
+      accelerometer; gyroscope; picture-in-picture; encrypted-media" data-testid="embed-iframe" frameborder="0" scrolling="no" allowfullscreen></iframe>
+      <p>Stay safe Johnketti bye!</p>
+      </div>
+  )
+}
